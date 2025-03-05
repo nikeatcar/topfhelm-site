22b5c36aa2954e2e3aab65477b1df4f71b023b2b
nikeatcar <ssrnikita@gmail.com>
2025-03-05 15:33:25 +0300

    Оптимизация стилей

--- script.js
+++ script.js
@@ -51,6 +51,21 @@ if (presaveBtn) {
     }
     });
 
+    document.addEventListener("DOMContentLoaded", function () {
+        document.querySelectorAll(".youtube-placeholder").forEach(container => {
+            container.addEventListener("click", function () {
+                const videoId = this.getAttribute("data-video");
+                const iframe = document.createElement("iframe");
+                iframe.setAttribute("src", `https://www.youtube.com/embed/${videoId}?autoplay=1`);
+                iframe.setAttribute("frameborder", "0");
+                iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
+                iframe.setAttribute("allowfullscreen", "");
+                this.innerHTML = "";
+                this.appendChild(iframe);
+            });
+        });
+    });
+
     // Параллакс-эффект для фона
     window.addEventListener("scroll", function () {
         let scrollTop = window.scrollY;
